9ec9d0b3f2e8357e6e545494091929c6c369c7a8
Denys Volodko <babyleejane@gmail.com>
2022-12-21 19:38:20 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -4,20 +4,14 @@ const refs = {
   form: document.querySelector('form'),
 };
 
-const formData = {
-  delay: 0,
-  step: 0,
-  amount: 0,
-};
-
 refs.form.addEventListener('submit', onFormSubmit);
 
 function onFormSubmit(e) {
   e.preventDefault();
-  collectData(e);
-  for (let index = 0; index < formData.amount; index++) {
-    const delay = Number(formData.delay) + Number(formData.step) * index;
-    createPromise(index + 1, delay)
+  const { delay, step, amount } = e.currentTarget.elements;
+  for (let index = 0; index < amount.value; index++) {
+    const delayValue = Number(delay.value) + Number(step.value) * index;
+    createPromise(index + 1, delayValue)
       .then(({ position, delay }) => {
         Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, {
           useIcon: false,
@@ -29,6 +23,7 @@ function onFormSubmit(e) {
         });
       });
   }
+  e.target.reset();
 }
 
 function createPromise(position, delay) {
@@ -43,15 +38,3 @@ function createPromise(position, delay) {
     }, delay);
   });
 }
-
-function collectData(e) {
-  const {
-    elements: { delay, step, amount },
-  } = e.currentTarget;
-  formData.delay = delay.value;
-  formData.step = step.value;
-  formData.amount = amount.value;
-}
-
-// Створити функцію яка буде створювати проміс.
-// Створити функцію яка буде створювати масив промісів з різними ділей і позиціями
